39ff06016ffb7b09617599f5bb9d121d767ce7da
TrimbleGuan <1366355241@qq.com>
2020-09-15 11:21:35 +0800

    trimble

--- app/src/main/java/com/haitian/mattress/activity/LoginActivity.java
+++ app/src/main/java/com/haitian/mattress/activity/LoginActivity.java
@@ -16,6 +16,6 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        TextView zhu = findViewById(R.id.zhu);
+//        TextView zhu = findViewById(R.id.zhu);
     }
 }
